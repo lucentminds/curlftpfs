ca5404181e3d332c9535c407f38c8c93fbdeaa8b
lucentminds <scott@lifecorp.com>
2017-10-06 19:13:58 -0500

    Successfully create new build structure!

--- build.sh
+++ build.sh
@@ -6,8 +6,15 @@ function fail ()
    exit 1
 }
 
+function echo_yellow
+{
+   # http://misc.flogisoft.com/bash/tip_colors_and_formatting
+   echo -e "\e[33m$1\e[0m"
+} #/echo_yellow()
+
 ROOT_DIR=$(pwd)
 
+mkdir ./build
 cp -r ./src/* ./build || fail
 cd ./build || fail
 libtoolize || fail
@@ -18,4 +25,5 @@ automake --add-missing || fail
 ./configure || fail
 make || fail
 
-printf "\nNow run make install.\n"
+echo -e "\nNow run: "
+echo_yellow "sudo make -C ./build install\n"
